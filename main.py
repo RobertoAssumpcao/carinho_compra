7b0a89e4287fe91a380016be433313788b6a7fc0
Roberto de Paiva Assumpção <robertoassumpcao1999@gmail.com>
2021-10-17 23:37:09 -0300

    output e  quantidade carinho
    Melhorando output e logica da quantidade do carrinho

--- main.py
+++ main.py
@@ -1,27 +1,50 @@
+"""
+Criar uma lista de carinhos e criar um objeto para cada produto.
+Criar funcao adicionar, excluir e editar
+"""
 # Declarando variaveis necessarias
 fechamento_carinho = False
-carrinho_pagamento = 0
+carrinho_pagamento = 0    
+
+# imprimindo tabela
+print("----------*Tabela preços*----------\ncodigo - produto - preço\n100 - Hot-dog - $1.20\n101 - Bauro - $1.30\n102 - Bauro com Ovo - $1.50\n")
 
 #repeticao até o fechamento do carrinho
 while fechamento_carinho == False:
-    #Coletando codigo e quantidade
-    codigo_produto = int(input("Digite o codigo do produto\n"))
-    quantidade_produto = int(input("Digite a quantidade\n"))
+    #Coletando codigo
+    codigo_produto = int(input("Digite o codigo do produto:\n"))
 
     # verificando o que será acrescentado no carrinho
     if codigo_produto == 100:
-        print("hot-dog")
+        # Coletando quantidade de do produto
+        quantidade_produto = int(input("Digite a quantidade:\n"))
+        
+        print("hot-dog adicionando com sucesso!")
         carrinho_pagamento = carrinho_pagamento +  (1.20 * quantidade_produto)
+        print(f'O valor do carrinho está em ${carrinho_pagamento:.2f}')
+        
     elif codigo_produto == 101:
-        print("Bauru")
+        # Coletando quantidade de do produto
+        quantidade_produto = int(input("Digite a quantidade:\n"))
+        
+        print("Bauru adicionando com sucesso!")
         carrinho_pagamento = carrinho_pagamento +  (1.30 * quantidade_produto)
+        print(f'O valor do carrinho está em ${carrinho_pagamento:.2f}')
+        
     elif codigo_produto == 102:
-        print("Bauru com Ovo")
+        # Coletando quantidade de do produto
+        quantidade_produto = int(input("Digite a quantidade:\n"))
+        
+        print("Bauru com Ovo adicionando com sucesso!")
         carrinho_pagamento = carrinho_pagamento + (1.50 * quantidade_produto)
+        print(f'O valor a ser pago está em ${carrinho_pagamento:.2f}')
+        
+    else:
+        print("Codigo invalido")
     
     # verificando o fechamento do carrinho
-    aux_fechamento_carrinho = input("Deseja fechar o carrinho, digite Y para fechar ?\n")   
+    aux_fechamento_carrinho = input("Deseja fechar o carrinho ? Digite Y para fechar\n")   
     if aux_fechamento_carrinho == "Y":
         fechamento_carinho = True
     
-print(carrinho_pagamento)
+print(f'O valor a ser pago é ${carrinho_pagamento:.2f}\n')
